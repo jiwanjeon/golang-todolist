34d9ff25d4e7dc40b297963ad05eb353ec6a74e7
jjeon17 <33076677+Jonathan940110@users.noreply.github.com>
2022-02-26 21:03:28 +0900

    Add: TestGetTodoById Unit Test Code

--- pkg/routes/routes_test.go
+++ pkg/routes/routes_test.go
@@ -1,6 +1,7 @@
 package routes
 
 import (
+	// "fmt"
 	"net/http/httptest"
 	"testing"
 	"net/http"
@@ -13,20 +14,43 @@ func TestGetTodo(t *testing.T) {
 	if err != nil {
 		t.Fatal(err)
 	}
-	rr := httptest.NewRecorder()
+	ResponseRecorder := httptest.NewRecorder()
 	router := mux.NewRouter()
 	Routes(router)
-	router.ServeHTTP(rr, req)
+	router.ServeHTTP(ResponseRecorder, req)
 
   // Check the status code is what we expect.
-  if status := rr.Code; status != http.StatusOK {
+  if status := ResponseRecorder.Code; status != http.StatusOK {
       t.Errorf("handler returned wrong status code: got %v want %v",
           status, http.StatusOK)
 	}
 	
 	expected := `[{"ID":46,"CreatedAt":"2022-02-25T11:17:56.93215+09:00","UpdatedAt":"2022-02-25T11:17:56.93215+09:00","DeletedAt":null,"title":"test-title-console-check","description":"test-description-console-check","conditions":true}]`
-	if rr.Body.String() != expected {
+	if ResponseRecorder.Body.String() != expected {
 		t.Errorf("handler returned unexpected body: got %v want %v",
-			rr.Body.String(), expected)
+		ResponseRecorder.Body.String(), expected)
 	}
+}
+
+func TestGetTodoById(t *testing.T) {
+	req, err := http.NewRequest("GET", "/todo/46", nil)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	ResponseRecorder := httptest.NewRecorder()
+	router := mux.NewRouter()
+	Routes(router)
+	router.ServeHTTP(ResponseRecorder, req)
+
+	if status := ResponseRecorder.Code; status != http.StatusOK {
+		t.Errorf("handler returned wrong status code: got %v want %v",
+				status, http.StatusOK)
+}
+
+expected := `{"ID":46,"CreatedAt":"2022-02-25T11:17:56.93215+09:00","UpdatedAt":"2022-02-25T11:17:56.93215+09:00","DeletedAt":null,"title":"test-title-console-check","description":"test-description-console-check","conditions":true}`
+if ResponseRecorder.Body.String() != expected {
+	t.Errorf("handler returned unexpected body: got %v want %v",
+	ResponseRecorder.Body.String(), expected)
+}
 }
