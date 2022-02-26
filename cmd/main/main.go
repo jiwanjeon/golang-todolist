48650709c39e34f81587f55dffec0f9866d4c724
jjeon17 <33076677+Jonathan940110@users.noreply.github.com>
2022-02-26 21:14:29 +0900

    Delete: Comment lines & fmt

--- cmd/main/main.go
+++ cmd/main/main.go
@@ -14,11 +14,8 @@ import (
 )
 
 func main() {
-	
 	migrate := flag.Bool("fork", true, "bool")
-	
 	if *migrate == true {
-		fmt.Println("Migration Working...")
 		models.Migrate()
 		router := mux.NewRouter()
 		routes.Routes(router)
@@ -27,24 +24,5 @@ func main() {
 	} else {
 		fmt.Println("You should parsing argument with migrate")
 	}
-	
+
 }
-
-// TODO : Complete 와 InComplete가 하는 동작이 똑같은데 하나로 합칠 수 있을듯?
-// 아 아니다 프론트 쪽에서 complete 인지 InComplete 인지 상태를 개별적으로 쏴주기 때문에 api관리를 따로 하는게 맞는듯?
-// 아니지 그게 바꾸는게 Update니까 말 그대로 업데이트에 넣어주면 되지 않을까?
-// 상황에 따라 따로 뺼수도 있음(사용자가 특정 API Request가 많은 경우 따로 관리해주는게 좋음)
-
-
-// TODO with 종민님
-
-/*
-	> C.R.U.D Interface 
-		
-		- Method 명 동사로
-		- 모델 객체를 목적어가 아닌 주어로 생각하여 함수 작성 (CreateTodo --> todo *Todo)
-		- Migration의 경우 수동으로 하는게 좋음(데이터 손실 방지)
-	
-	> 변수 이름
-		- 객체 이름은 명사형
-*/
